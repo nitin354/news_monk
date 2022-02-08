80c84acd74b9e9ade8220b8276467a877ece4659
nitin354 <73094335+nitin354@users.noreply.github.com>
2022-02-08 19:29:22 +0530

    new comit

--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Newsitem from './Newsitem';
 
+import loder from '../loadern.gif';
+
 export class News extends Component {
 
     article = [];
@@ -9,7 +11,7 @@ export class News extends Component {
         super();
         this.state = {
             article: this.article,
-            loader: false,
+            loader: true,
             page:1
         }
     }
@@ -19,35 +21,30 @@ async componentDidMount(){
     let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=f639a9c6417b472ba6c24967cdbde4b1";
     let data = await fetch(url);
     let parsedata = await data.json();
+    
 
-    this.setState({article: parsedata.articles,totalResults:parsedata.totalResults});
+    setTimeout(() => this.setState({article: parsedata.articles,totalResults:parsedata.totalResults,loader:false}), 2000); 
+    //this.setState({article: parsedata.articles,totalResults:parsedata.totalResults,loader:false});
      
 }
 
 handlePrevNews = async () => {
 
-    console.log("Left");
+   // console.log("Left");
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f639a9c6417b472ba6c24967cdbde4b1&page=${this.state.page - 1}&pageSize=20`;
     let data = await fetch(url);
     let parsedata = await data.json();
-
-  this.setState({
-    article : parsedata.articles,
-    page : this.state.page - 1
-  })
+    this.setState({
+        article : parsedata.articles,
+        page : this.state.page - 1
+    })
 
 }
 
 
-
-
 handleNextNews = async () => {
 
-    // if(Math.ceil(this.state.totalResults/20) < this.state.page+1){
-
-    // }else{
-
-        console.log("Right");
+        //console.log("Right");
         let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=f639a9c6417b472ba6c24967cdbde4b1&page=${this.state.page + 1}&pageSize=20`;
         let data = await fetch(url);
         let parsedata = await data.json();
@@ -56,10 +53,6 @@ handleNextNews = async () => {
         article : parsedata.articles,
         page : this.state.page + 1
       })
-
-    // }
-
-
     
 }
 
@@ -69,19 +62,18 @@ handleNextNews = async () => {
         return (
             <div className="container my-2">
 
-                <div className="row" >
-                    {this.state.article.map((element) => {
-                        return<div className="col-md-4 my-2" key={element.url}>
+            {this.state.loader?<div className="row"><div className="col-md-4 my-2"></div><div className="col-md-4 my-2"><img src={loder} className="text-center my-4" /></div><div className="col-md-4 my-2"></div></div>:<><div className="row" >
+            
+                    {this.state.article.map((element) => 
+                       <div className="col-md-4 my-2" key={element.url}>
                                 <Newsitem title={element.title?element.title.slice(0,44):""} description={element.description?element.description.slice(0,60):""} url={element.url} imgurl={element.urlToImage} />
                             </div>
-                    })}
-
+                    )}
                 </div>
-
                 <div className="container my-2 d-flex justify-content-between">
                 <button disabled={this.state.page<=1} className="btn btn-dark" onClick={this.handlePrevNews} >Previous</button>
                 <button disabled={Math.ceil(this.state.totalResults/20) < this.state.page+1} className="btn btn-dark" onClick={this.handleNextNews} >Next</button>
-                </div>
+                </div></>}
             </div>
         )
     }
